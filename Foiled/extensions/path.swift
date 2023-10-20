8d9877fb1f2b4c509ec9c5868b9a9c3ff3025c42
MuhammadMosaBinSaghir <member_wake_0z@icloud.com>
2023-10-20 11:26:15 -0400

    Reworking bumps

---
+++ Foiled/extensions/path.swift
@@ -0,0 +1,8 @@
+//
+//  path.swift
+//  Foiled
+//
+//  Created by Muhammad Saghir on 2023-10-18.
+//
+
+import Foundation
---
+++ Foiled/extensions/size.swift
@@ -0,0 +1,8 @@
+//
+//  size.swift
+//  Foiled
+//
+//  Created by Muhammad Saghir on 2023-10-18.
+//
+
+import Foundation
